df07487a211c67a1122be53e5370e4e23f523c01
Jacques Olivier <jacqueso.olivier@gmail.com>
2024-06-08 14:25:05 +1200

    feat: add example page

---
+++ src/app/features/example/ExamplePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const ExamplePage = () => {
+    return <>
+    <Container>
+        <h1>This is an Example Page</h1>
+    </Container>
+    </>
+}
+
+export default ExamplePage;
