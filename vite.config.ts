077dfea0443eb2d69bca4d618a6d23f8f06241af
Jacques Olivier <jacqueso.olivier@gmail.com>
2024-06-11 18:50:14 +1200

    build: vitest & husky 🐶

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000,
-    // If you want to enable HTTPS,
-    // 1. generate certificates (like mkcert)
-    // 2. Add the certificates to the root folder
-    // https: {
-    //   key: fs.readFileSync('./localhost+2-key.pem'),
-    //   cert: fs.readFileSync('./localhost+2.pem'),
-    // },
-    open: true,
-    cors: true,
-    // ... other server options if needed
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Adjust the path as needed
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      host: 'localhost',
+      port: 3000,
+      // If you want to enable HTTPS,
+      // 1. generate certificates (like mkcert)
+      // 2. Add the certificates to the root folder
+      // https: {
+      //   key: fs.readFileSync('./localhost+2-key.pem'),
+      //   cert: fs.readFileSync('./localhost+2.pem'),
+      // },
+      open: true,
+      cors: true,
+      // ... other server options if needed
     },
-  },
+    resolve: {
+      alias: {
+        '@': '/src', // Adjust the path as needed
+      },
+    },
+    define: {
+      'process.env': env, // Expose environment variables to your code
+    },
+  };
 });
