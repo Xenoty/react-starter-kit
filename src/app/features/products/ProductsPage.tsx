8d6f5ec633e4e40e95603589080191c6a5880043
Jacques Olivier <jacqueso.olivier@gmail.com>
2024-06-15 16:29:45 +1200

    style: add skeleton loading 🦴

--- src/app/features/products/ProductsPage.tsx
+++ src/app/features/products/ProductsPage.tsx
@@ -1,33 +1,53 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { Product } from '../../../domain/entities/Product';
 import { getAllProducts } from '../../../data/api/endpoints/product.endpoint';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import './ProductsPage.scss';
 
 const ProductsPage: React.FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [products, setProducts] = useState<Product[]>();
 
   const fetchProducts = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const productsResponse = await getAllProducts({ limit: 10 });
-      setProducts(productsResponse.products);
-    } catch (error) {
-    } finally {
-      setIsLoading(false);
-    }
+    setTimeout(async () => {
+      setIsLoading(true);
+      try {
+        const productsResponse = await getAllProducts({ limit: 10 });
+        setProducts(productsResponse.products);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    }, 2500);
   }, []);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  console.log(isLoading);
+  const Products = () => {
+    if (isLoading) {
+      return (
+        <>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <Col key={index}>
+              <Card>
+                <Skeleton height={20} /> {/* Header skeleton */}
+                <Skeleton height={150} /> {/* Image skeleton */}
+                <Card.Body>
+                  <Skeleton count={4} /> {/* Title and description skeletons */}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </>
+      );
+    }
 
-  return (
-    <Container className="justify-content-center d-flex">
-      <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+    return (
+      <>
         {products?.map((product, index) => (
           <Col key={index}>
             <Card>
@@ -40,6 +60,16 @@ const ProductsPage: React.FC = () => {
             </Card>
           </Col>
         ))}
+      </>
+    );
+  };
+
+  console.log(isLoading);
+
+  return (
+    <Container className="justify-content-center">
+      <Row xs={2} sm={2} md={3} lg={4} className="g-4">
+        <Products />
       </Row>
     </Container>
   );
