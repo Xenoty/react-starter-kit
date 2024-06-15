514d3a48a140dad0779c88b88bc350493475a1db
Jacques Olivier <jacqueso.olivier@gmail.com>
2024-06-15 17:10:20 +1200

    style: add infinite scrolling! 🤯

--- src/app/features/products/ProductsPage.tsx
+++ src/app/features/products/ProductsPage.tsx
@@ -1,34 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { Product } from '../../../domain/entities/Product';
 import { getAllProducts } from '../../../data/api/endpoints/product.endpoint';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Form, Row } from 'react-bootstrap';
 import './ProductsPage.scss';
 
 const ProductsPage: React.FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [products, setProducts] = useState<Product[]>();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [page, setPage] = useState<number>(1); // Track the current page
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [searchTerm, setSearchTerm] = useState(''); // State for the search term
+  const initialProductsFetched = useRef(false); // Ref to track initial fetch
+
+  const filteredProducts = products?.filter((product) => {
+    return product.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   const fetchProducts = useCallback(async () => {
     setTimeout(async () => {
-      setIsLoading(true);
       try {
-        const productsResponse = await getAllProducts({ limit: 10 });
-        setProducts(productsResponse.products);
+        const productsResponse = await getAllProducts({
+          limit: 10,
+          skip: (page - 1) * 10, // Calculate skip based on the page
+        });
+        setProducts((prevProducts) => [
+          ...prevProducts,
+          ...productsResponse.products,
+        ]);
+        setHasMore(productsResponse.products.length > 0);
       } catch (error) {
+        console.error('Error fetching products:', error);
       } finally {
-        setIsLoading(false);
+        if (!initialProductsFetched.current) {
+          initialProductsFetched.current = true;
+        }
       }
     }, 2500);
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
+
+  const fetchData = () => {
+    if (!searchTerm && hasMore) {
+      setPage((prevPage) => prevPage + 1); // Load next page
+    }
+  };
 
   const Products = () => {
-    if (isLoading) {
+    if (initialProductsFetched.current === false) {
       return (
         <>
           {Array.from({ length: 12 }).map((_, index) => (
@@ -48,7 +71,7 @@ const ProductsPage: React.FC = () => {
 
     return (
       <>
-        {products?.map((product, index) => (
+        {filteredProducts.map((product, index) => (
           <Col key={index}>
             <Card>
               <Card.Header as="h5">{product.price}</Card.Header>
@@ -64,13 +87,46 @@ const ProductsPage: React.FC = () => {
     );
   };
 
-  console.log(isLoading);
-
   return (
     <Container className="justify-content-center">
-      <Row xs={2} sm={2} md={3} lg={4} className="g-4">
-        <Products />
-      </Row>
+      <Form.Control
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+          setHasMore(!e.target.value && initialProductsFetched.current); // Update hasMore if search is cleared
+        }}
+        className="mb-3" // Add some margin-bottom
+      />
+
+      <InfiniteScroll
+        dataLength={filteredProducts.length}
+        next={fetchData}
+        hasMore={hasMore}
+        loader={
+          <Row xs={2} sm={2} md={3} lg={4} className="g-4">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <Col key={index}>
+                <Card style={{ width: '18rem' }}>
+                  <Skeleton height={20} />
+                  <Skeleton height={150} />
+                  <Card.Body>
+                    <Skeleton count={4} />
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        } // Skeleton while loading
+        endMessage={
+          <p style={{ textAlign: 'center' }}>Yay! You have seen it all</p>
+        }
+      >
+        <Row xs={2} sm={2} md={3} lg={4} className="g-4">
+          <Products />
+        </Row>
+      </InfiniteScroll>
     </Container>
   );
 };
